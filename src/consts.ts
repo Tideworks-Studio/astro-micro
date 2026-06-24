2c136d05a9eaa775af0107a03560f6d6c229a781
marsy <amara@rsnu.link>
2026-06-23 20:42:30 -0400

    hm

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
+  TITLE: "Anchor & Comapss",
   DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  EMAIL: "blog@tideworks.studio",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
